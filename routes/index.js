2acd794373c0e3111d0e9a44db01d1156724b996
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-04-12 13:48:43 -0300

    login/sign up page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'EasyCooking' });
 });
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
 
 module.exports = router;
