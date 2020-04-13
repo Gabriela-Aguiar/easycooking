c0b6ef81c03674db619332d90fc304bf4d412acc
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-04-13 09:21:07 -0300

    Add dotenv, update login.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const token = process.env['API_TOKEN']
+const apiUrl = `https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${token}`
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -9,6 +11,7 @@ router.get('/', (req, res, next) => {
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
 
 //trying to get the results from the API
 // let data = {
