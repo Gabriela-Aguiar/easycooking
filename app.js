066d7d0b822024bb58595d86fe4ec0674f0f250a
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-04-13 10:43:41 -0300

    Add signup route with password using bcrypt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 var createError = require( 'http-errors' );
 var express = require( 'express' );
 var path = require( 'path' );
+const hbs = require('hbs');
 var cookieParser = require( 'cookie-parser' );
 var logger = require( 'morgan' );
 var app = express();
