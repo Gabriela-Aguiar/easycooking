a7ff2806bcae95b9ee9492df86965f27f8804bbe
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-04-15 14:30:37 -0300

    add getrecipes route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ var express = require( 'express' );
 var router = express.Router();
 const token = process.env[ 'API_TOKEN' ]
 const apiUrl = `https://api.spoonacular.com/recipes/search?query=`
-const apiUrlFinal = `&number=2&apiKey=${token}`
+const apiUrlFinal = `&number=10&apiKey=${token}`
 
 const User = require( '../Models/users' )
 const axios = require( 'axios' );
@@ -20,53 +20,53 @@ router.get( '/allrecipes', ( req, res ) => {
 } )
 
 
-// router.get( '/teste', ( req, res ) => {
-//   let ingredient = req.query.ingredients
+router.get( '/teste', ( req, res ) => {
+  let ingredient = req.query.ingredients
 
-//   // console.log( ingredient )
-//   const apiUrl = `https://api.spoonacular.com/recipes/search?query=${ingredient[0]}&query=${ingredient[1]}&query=${ingredient[2]}${apiUrlFinal}`
-//   console.log( apiUrl )
+  // console.log( ingredient )
+  const apiUrl = `https://api.spoonacular.com/recipes/search?query=${ingredient[0]}&query=${ingredient[1]}&query=${ingredient[2]}${apiUrlFinal}`
+  console.log( apiUrl )
 
-//   axios.get( apiUrl )
-//     .then( resp => {
-//       resp.data.results.forEach( recipe => {
-//         const {
-//           title,
-//           readyInMinutes,
-//           image,
-//           id
-//         } = recipe
+  axios.get( apiUrl )
+    .then( resp => {
+      resp.data.results.forEach( recipe => {
+        const {
+          title,
+          readyInMinutes,
+          image,
+          id
+        } = recipe
 
-//         //check if id is already in the db
-//         if ( recipeIdsArr.includes( id ) ) return
+        //check if id is already in the db
+        if ( recipeIdsArr.includes( id ) ) return
 
-//         Recipe.create( {
-//           title,
-//           readyInMinutes,
-//           image,
-//           id
-//         } )
-//         recipeIdsArr.push( id )
-//         console.log( 'receita criada com sucesso' );
-//       } )
-//       res.render( 'all' )
-//     } )
-//     .catch( err => console.log( err ) )
-// } )
+        Recipe.create( {
+          title,
+          readyInMinutes,
+          image,
+          id
+        } )
+        recipeIdsArr.push( id )
+        console.log( 'receita criada com sucesso' );
+      } )
+      res.render( 'all' )
+    } )
+    .catch( err => console.log( err ) )
+} )
 
 router.get( '/getrecipes', ( req, res ) => {
   let ingredient = req.query.ingredients;
-  let searchedId = {};
+  let searchedId = [];
 
   // console.log( ingredient )
   const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient[0]},+${ingredient[1]},+${ingredient[2]}${apiUrlFinal}`
-  
+
   console.log( apiUrl )
 
   axios.get( apiUrl )
     .then( resp => {
       resp.data.forEach( recipe => {
-        console.log(recipe);
+        console.log( recipe );
         const {
           title,
           image,
@@ -74,8 +74,8 @@ router.get( '/getrecipes', ( req, res ) => {
           likes
         } = recipe
 
-       searchedId = {id};
-        console.log('---------->', searchedId)
+        searchedId.push( id );
+        console.log( '---------->', searchedId )
 
         //check if id is already in the db
         if ( recipeIdsArr.includes( id ) ) return
@@ -88,14 +88,17 @@ router.get( '/getrecipes', ( req, res ) => {
         } )
         recipeIdsArr.push( id )
         // console.log( 'receita criada com sucesso' );
-      })
-      getRecipes.find({searchedId})
-      .then( resp => {
-        res.render('all', {getRecipes: resp})
-      })
+      } )
+      getRecipes.find( {
+          $or:[{id: searchedId}]  
+        } )
+        .then( resp => {
+          console.log( resp )
+          console.log( searchedId )
+          res.render( 'all', resp )
+        } )
     } )
     .catch( err => console.log( err ) )
 } )
-
 
 module.exports = router;
