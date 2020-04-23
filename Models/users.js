942358d0c19567e74e9a43f6d69c47087a5cbe3f
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-04-23 16:12:57 -0300

    email and username unique

--- Models/users.js
+++ Models/users.js
@@ -3,13 +3,15 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: {
-    type: String
+    type: String,
+    unique:true
   },
   password: {
     type: String
   },
   email: {
-    type: String
+    type: String,
+    unique:true
   },
   name: {
     type: String
