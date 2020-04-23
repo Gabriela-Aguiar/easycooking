971d92308090dc41bf37d0cca558f3877be2a813
Gabriela Aguiar <aguiar.gabi95@gmail.com>
2020-04-23 15:30:37 -0300

    reset password button in email

--- templates/resetTemplate.js
+++ templates/resetTemplate.js
@@ -204,7 +204,7 @@ const resetTemplate = (id, username ) => {
       <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
         <tr>
           <td align="center" bgcolor="#F1935C" role="presentation" style="border:none;border-radius:3px;cursor:auto;mso-padding-alt:10px 25px;background:#F1935C;" valign="middle">
-            <a href="http://localhost:3000/resetPassword/${id}" style="display: inline-block; background: #F1935C; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; line-height: 120%; margin: 0; text-transform: none; padding: 10px 25px; mso-padding-alt: 0px; border-radius: 3px; text-decoration: none; color: inherit;" target="_blank">
+            <a href="https://easy-cooking-app.herokuapp.com/resetPassword/${id}" style="display: inline-block; background: #F1935C; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; line-height: 120%; margin: 0; text-transform: none; padding: 10px 25px; mso-padding-alt: 0px; border-radius: 3px; text-decoration: none; color: inherit;" target="_blank">
               Reset password
             </a>
           </td>
